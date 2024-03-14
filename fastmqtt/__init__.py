2f1fe8d44ac9d432e34e46b2747ea3351f3e62dd
toxazhl <zhltoxa@gmail.com>
2024-03-14 06:03:54 +0200

    add external imports

--- fastmqtt/__init__.py
+++ fastmqtt/__init__.py
@@ -1,9 +1,12 @@
+from aiomqtt import Message
+
 from .exceptions import FastMQTTError
 from .fastmqtt import FastMQTT
 from .router import MQTTRouter
 from .subscription_manager import Retain, Subscription
 
 __all__ = [
+    "Message",
     "FastMQTT",
     "MQTTRouter",
     "FastMQTTError",
