79c624cc7c9d37417c14c90513b27ba3941f157d
toxazhl <zhltoxa@gmail.com>
2024-08-15 22:43:50 +0300

    add request context example

--- example/request_response.py
+++ example/request_response.py
@@ -1,23 +1,23 @@
-# import asyncio
+import asyncio
 
-# from fastmqtt import FastMqtt, Message, MqttRouter
+from fastmqtt import FastMqtt, Message, MqttRouter
 
-# router = MqttRouter()
-
-
-# @router.on_message("temperature/request")
-# async def temp_request_handler(message: Message):
-#     # Simulate getting a temperature reading
-#     return 25  # Return the temperature
+router = MqttRouter()
 
 
-# async def main():
-#     async with FastMqtt("test.mosquitto.org", routers=[router]) as fastmqtt:
-#         ...
-#         async with fastmqtt.response_context(f"temperature/response/{fastmqtt.identifier}") as ctx:
-#             response = await ctx.request("temperature/request")
-#             print(f"Temperature: {response.payload.text()}")
+@router.on_message("temperature/request")
+async def temp_request_handler(message: Message):
+    # Simulate getting a temperature reading
+    return 25  # Return the temperature
 
 
-# if __name__ == "__main__":
-#     asyncio.run(main())
+async def main():
+    async with FastMqtt("test.mosquitto.org", routers=[router]) as fastmqtt:
+        ...
+        async with fastmqtt.response_context(f"temperature/response/{fastmqtt.identifier}") as ctx:
+            response = await ctx.request("temperature/request")
+            print(f"Temperature: {response.payload.text()}")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
